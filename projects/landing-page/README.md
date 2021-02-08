82ce44efd1b24db3b702a88af911d238db434faf
Sohila Khaled <sohila.khaled@orchtech.com>
2021-02-08 11:33:09 +0200

    init commit

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -3,11 +3,23 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [Index.html](#index)
+* [App.js](#app)
+* [Styles.css](#style)
+* [Running](#running)
 
 ## Instructions
 
 The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Index
+This file represents the skeleton of the landing page. Within the header, you'll find the navigation outlined. Within the body, there are five sections that are added to begin with. Right now, there is simply filler text here but this can be easily fixed in the future.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## App
+Here is where the functionality happens. There is three things that this file primarily achieves. The first is dynamically building out the navigation showing which section is currently "active" in the page. listens for clicks on the navigation and then scrolls the page to the appropriate section header.
+
+## Styles
+Most of the code here was taken from the skelton code provided by Udacity. This gives the landing page the look and feel that you see and experience. One notable thing is the hover animation on the active navigation header.
+
+## Running
+To launch this website, simply clone the repo and open the `index.html` file in the browser. 
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,41 +17,108 @@
  * Define Global Variables
  * 
 */
-
-
+const Navbar_menu = document.querySelector('#navbar__list');
+const Content_Sections = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+function IsVisible() {
+    VisiSection = Content_Sections[0];
+    minVal = 1000000;
+    for (item of Content_Sections) {
+        let bounding = item.getBoundingClientRect();
+        if (bounding.top > -300 & bounding.top < minVal) {
+            minVal = bounding.top;
+            VisiSection = item;
+        };
+    };
+    return VisiSection;
 
 
-
+}
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
-
 // build the nav
-
+function Build_Nav() {
+    for (let i = 0; i < Content_Sections.length; i++) {
+        let navitems = document.createElement('li');
+        navitems.classList = 'menu__link';
+        navitems.setAttribute('data-section-id', Content_Sections[i].id);
+        navitems.innerText = `${Content_Sections[i].id}`;
+        Navbar_menu.appendChild(navitems);
+    };
+};
+//Responsive Mobile Menu
+function MobileMenu() {
+    if (Navbar_menu.className === "topnav") {
+        Navbar_menu.classList.add('responsive');
+    } else {
+        Navbar_menu.className = "topnav";
+    }
+}
 
 // Add class 'active' to section when near top of viewport
+function SetSectionsActive() {
+    window.addEventListener('scroll', function () {
+        let ActiveSection = IsVisible();
+        ActiveSection.classList.add('your-active-class');
+        for (let i = 0; i < Content_Sections.length; i++) {
+            if (Content_Sections[i].id != ActiveSection.id & Content_Sections[i].classList.contains('your-active-class')) {
+                Content_Sections[i].classList.remove('your-active-class');
+            }
+        }
+        const ActiveNavItems = document.querySelectorAll('li');
+        for (let i = 0; i < ActiveNavItems.length; i++) {
+            if (ActiveNavItems[i].getAttribute('data-section-id') == ActiveSection.id) {
+                ActiveNavItems[i].classList.add('activelink');
+            }
+            else {
+                ActiveNavItems[i].classList.remove('activelink');
+            }
+        }
+    });
+};
 
 
 // Scroll to anchor ID using scrollTO event
+function ScrollToSection() {
 
+    Navbar_menu.addEventListener('click', function (event) {
+        const clicked = document.querySelector('#' + event.target.dataset.sectionId);
+        clicked.scrollIntoView({ 'behavior': 'smooth' });
 
+    });
+};
+
+//Scroll to Top function
+let TopButton = document.getElementById("topBtn");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        TopButton.style.display = "block";
+    } else {
+        TopButton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 /**
  * End Main Functions
  * Begin Events
  * 
 */
 
-// Build menu 
-
+// Build menu
+Build_Nav();
 // Scroll to section on link click
-
+ScrollToSection();
 // Set sections as active
-
+SetSectionsActive();
 
